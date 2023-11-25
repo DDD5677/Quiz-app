cb3f81a46c2ab2eb9a8efcb96d98d0324ab99e46
DDD5677 <djumaqulovdonisher@mail.ru>
2023-11-25 23:07:48 +0500

    connect mysql database

--- backend/main.js
+++ backend/main.js
@@ -3,16 +3,21 @@ const app = express();
 const mysql = require("mysql2");
 require("dotenv").config();
 
-// const db = mysql.createConnection({
-//    host: process.env.DB_HOST,
-//    user: process.env.DB_USER,
-//    password: process.env.DB_PASSWORD,
-//    database: process.env.DB_DATABASE,
-// });
+const pool = mysql.createPool({
+   host: process.env.DB_HOST,
+   user: process.env.DB_USER,
+   password: process.env.DB_PASSWORD,
+   database: process.env.DB_DATABASE,
+   port: process.env.DB_PORT,
+});
+const db = pool.promise();
+
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-   res.status(200).send("server is running ok");
+app.get("/", async (req, res) => {
+   const result = await db.query("SELECT * FROM test");
+   console.log(result);
+   res.status(200).send(result[0]);
 });
 
 app.listen(port, () => {

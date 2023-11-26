faf1c26c7401b9caa747a1018a68063654809d2c
DDD5677 <djumaqulovdonisher@mail.ru>
2023-11-26 22:24:29 +0500

    change database from mysql to mongodb

--- backend/main.js
+++ backend/main.js
@@ -1,24 +1,18 @@
 const express = require("express");
 const app = express();
-const mysql = require("mysql2");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
-const pool = mysql.createPool({
-   host: process.env.DB_HOST,
-   user: process.env.DB_USER,
-   password: process.env.DB_PASSWORD,
-   database: process.env.DB_DATABASE,
-   port: process.env.DB_PORT,
-});
-const db = pool.promise();
-
 const port = process.env.PORT || 3000;
-
-app.get("/", async (req, res) => {
-   const result = await db.query("SELECT * FROM test");
-   console.log(result);
-   res.status(200).send(result[0]);
-});
+const api = process.env.API_URL;
+mongoose
+   .connect(process.env.CONNECTING_STRING)
+   .then(() => {
+      console.log("Database connection is ready");
+   })
+   .catch((err) => {
+      console.log(err);
+   });
 
 app.listen(port, () => {
    console.log(`Server started on port ${port}`);
